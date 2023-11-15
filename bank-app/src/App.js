63c551009be1658e822902391d4be96fc993569e
jimmymakumi <jimmy.makumi@student.moringaschool.com>
2023-11-15 15:51:19 +0300

    completed

---
+++ bank-app/src/App.js
@@ -0,0 +1,69 @@
+import "./App.css"
+import { useState,useEffect } from 'react';
+import Header from './components/Header';
+import Transaction from './components/Transaction';
+import Filter from './components/Filter';
+
+function App() {
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    fetch('  https://my-json-server.typicode.com/stanleywanjau/bankof-flatiron/transactions')
+  .then(res => res.json())
+  .then(data => setTransactions(data));
+ 
+  }, []);
+  
+   function handleSubmit(FormData){
+    
+    fetch('https://my-json-server.typicode.com/stanleywanjau/bankof-flatiron/transactions',{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(FormData )
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to send data to the server.')
+      }
+      return response.json();
+    })
+    
+      .then((newTransaction) => {
+        setTransactions([...transactions, newTransaction]);
+      })
+      .catch(error => {
+      console.error('Error:', error);
+    });
+  
+}
+function deleteTransaction (transactionId){
+  
+  fetch(`https://my-json-server.typicode.com/stanleywanjau/bankof-flatiron/transactions/${transactionId}`, {
+    method: 'DELETE',
+    
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to delete the transaction from the server.');
+      }
+      setTransactions(transactions.filter((transaction) => transaction.id !== transactionId));
+    })
+    
+    .catch(error => {
+      console.error('Error:', error);
+    });
+};
+   
+  return (
+    <div className="App">
+      <h1> BANK OF FLATRION</h1>
+      <Header onSubmit={handleSubmit}/>
+      <Transaction Transactions={transactions} onDelete={deleteTransaction}/>
+      
+    </div>
+  );
+}
+
+export default App;
